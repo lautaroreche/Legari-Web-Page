1e7d90886b4128966f3a78f20f846f5a681fcea8
Lautaro Reche <lautaroreche1@gmail.com>
2025-07-12 19:06:51 +0100

    Fix redirect middleware route error

--- legari/settings.py
+++ legari/settings.py
@@ -77,7 +77,7 @@ if env("DEBUG", default=False):
     ]
 else:
     MIDDLEWARE = [
-        'legari_app.middleware.www_redirect.WWWRedirectMiddleware',
+        'legari_app.middleware.redirect',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
